020dfb06d1104c66bcfa4e3db49bbb7b8518de59
dingdayu <614422099@qq.com>
2019-01-21 02:52:22 +0800

    * remove debug code
    Signed-off-by: dingdayu <614422099@qq.com>

--- main.go
+++ main.go
@@ -1,15 +1,9 @@
 package main
 
-import (
-	"bytes"
-	"fmt"
-	"os/exec"
-	"strings"
-	"time"
-)
+import "adbs/api"
 
 func main() {
-	//api.Init()
+	api.Init()
 
 	//// adb version
 	//version, err := shell.Version()
@@ -29,52 +23,4 @@ func main() {
 	//	fmt.Println(err)
 	//}
 	//fmt.Printf("Packages.StartByIntent: %s\n", bo)
-
-	//阻塞直到该命令执行完成，该命令必须是被Start方法开始执行的
-	//cmd.Wait()
-
-	//stdin.Write([]byte("go text for grep\n"))
-	//stdin.Write([]byte("go test text for grep\n"))
-	//stdin.Close()
-	//
-	//out_bytes, _ := ioutil.ReadAll(stdout)
-	//
-	//if err := cmd.Wait(); err != nil {
-	//	fmt.Println("Execute failed when Wait:" + err.Error())
-	//	return
-	//}
-	//
-	//fmt.Println("Execute finished:" + string(out_bytes))
-
-	cmd := exec.Command("adb", "shell")
-	var pipIn, pipOut bytes.Buffer
-	cmd.Stdin = &pipIn
-	cmd.Stdout = &pipOut
-
-	if err := cmd.Start(); err != nil {
-		panic(err)
-	}
-
-	//result printer
-	go func() {
-		for {
-			line, _ := pipOut.ReadString('\n')
-			if line != "" {
-				fmt.Println(line)
-			}
-		}
-	}()
-
-	go cmd.Wait()
-	pipIn.WriteString(fmt.Sprintf("%s\n", "ls"))
-
-	time.Sleep(time.Millisecond * 5000)
-	fmt.Println(strings.Replace(pipOut.String(), "\n", "", 1))
-
-	var input string
-	fmt.Scanln(&input)
-
-	time.Sleep(time.Millisecond * 1000)
-	pipIn.WriteString(fmt.Sprintf("%s\n", "cd /"))
-	fmt.Println(strings.Replace(pipOut.String(), "\n", "", 1))
 }
