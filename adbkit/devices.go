90abd6f16cf27243ac8348118db505f9915e587a
dingdayu <614422099@qq.com>
2019-01-22 01:42:38 +0800

    * adbkit add Disconnect()
    Signed-off-by: dingdayu <614422099@qq.com>

--- adbkit/devices.go
+++ adbkit/devices.go
@@ -36,7 +36,7 @@ func (c Client) Devices() ([]Device, error) {
 	return nil, errors.New("error response: " + string(resp))
 }
 
-// 获取设备列表
+// 连接一个设备
 func (c Client) Connect(ip string, port int) (bool, error) {
 	resp, err := c.Command(fmt.Sprintf("host:connect:#%s:#%d", ip, port))
 	if err != nil {
@@ -47,11 +47,32 @@ func (c Client) Connect(ip string, port int) (bool, error) {
 
 		var res = strings.Trim(string(resp[8:8+length]), "\n")
 		if strings.Contains(res, "failed to connect") || strings.Contains(res, "unable to connect to") {
-			return false, errors.New(res)
+			return false, errors.New("failed to connect device")
 		}
 		if strings.Contains(res, "already connected to") || strings.Contains(res, "connected to") {
 			return true, nil
 		}
+	} else if string(resp[0:4]) == FAIL {
+		return false, errors.New("adb response: Fail")
+	}
+	return false, errors.New("error response: " + string(resp))
+}
+
+// 断开设备
+func (c Client) Disconnect(ip string, port int) (bool, error) {
+	resp, err := c.Command(fmt.Sprintf("host:disconnect:#%s:#%d", ip, port))
+	if err != nil {
+		return false, err
+	}
+	if string(resp[0:4]) == OKAY {
+		length, _ := strconv.Atoi(string(resp[4:8]))
+		fmt.Println(length)
+		fmt.Println(resp)
+		var res = strings.Trim(string(resp[8:8+length]), "\n")
+		if strings.Contains(res, "No such device") {
+			return false, errors.New("no such device")
+		}
+		return true, nil
 	} else if string(resp[0:4]) == FAIL {
 		return false, errors.New("adb response: Fail")
 	}
