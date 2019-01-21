d6185ebb9c41423ea4ca858c4ffcc81419611ef2
dingdayu <614422099@qq.com>
2019-01-22 01:08:54 +0800

    * adbkit add Connect()
    Signed-off-by: dingdayu <614422099@qq.com>

--- adbkit/devices.go
+++ adbkit/devices.go
@@ -2,6 +2,8 @@ package adbkit
 
 import (
 	"errors"
+	"fmt"
+	"strconv"
 	"strings"
 )
 
@@ -32,4 +34,26 @@ func (c Client) Devices() ([]Device, error) {
 		return nil, errors.New("adb response: Fail")
 	}
 	return nil, errors.New("error response: " + string(resp))
+}
+
+// 获取设备列表
+func (c Client) Connect(ip string, port int) (bool, error) {
+	resp, err := c.Command(fmt.Sprintf("host:connect:#%s:#%d", ip, port))
+	if err != nil {
+		return false, err
+	}
+	if string(resp[0:4]) == OKAY {
+		length, _ := strconv.Atoi(string(resp[4:8]))
+
+		var res = strings.Trim(string(resp[8:8+length]), "\n")
+		if strings.Contains(res, "failed to connect") || strings.Contains(res, "unable to connect to") {
+			return false, errors.New(res)
+		}
+		if strings.Contains(res, "already connected to") || strings.Contains(res, "connected to") {
+			return true, nil
+		}
+	} else if string(resp[0:4]) == FAIL {
+		return false, errors.New("adb response: Fail")
+	}
+	return false, errors.New("error response: " + string(resp))
 }
