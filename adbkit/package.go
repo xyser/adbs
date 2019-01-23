fd8c116dbf46a8dd2b3e659a49e5a4843a212a1a
dingdayu <614422099@qq.com>
2019-01-24 02:09:21 +0800

    * adbkit add Start()
    Signed-off-by: dingdayu <614422099@qq.com>

--- adbkit/package.go
+++ adbkit/package.go
@@ -188,3 +188,38 @@ func (c Client) UnInstall(serial, pkg string) (bool, error) {
 
 	return false, errors.New("adb response: " + string(resp))
 }
+
+// 打开 Activity or Service
+func (c Client) Start(serial, command string, args map[string]string) (bool, error) {
+	conn, err := c.Transport(serial)
+	if err != nil {
+		return false, err
+	}
+	// 准备读取返回
+	readChan := make(chan []byte)
+	go func() {
+		buf, _ := ioutil.ReadAll(conn)
+		readChan <- buf
+	}()
+
+	// 写入命令
+	command = fmt.Sprintf("shell:am %s", command)
+	_, err = conn.Write(EncodeCommend(command))
+	if err != err {
+		return false, err
+	}
+
+	resp := <-readChan
+	if string(resp[0:4]) == OKAY {
+		var re = regexp.MustCompile(`(?m)^Error: (.*)$`)
+		match := re.FindStringSubmatch(string(resp[0:4]))
+		if len(match) > 1 {
+			return false, errors.New(match[1])
+		}
+		return true, nil
+	} else if string(resp[0:4]) == FAIL {
+		return false, errors.New("adb fail response: " + string(resp[4:]))
+	}
+
+	return false, errors.New("adb response: " + string(resp))
+}
