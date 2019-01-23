d3dd359fee9680432311cb17b5df2e4a961d39bb
dingdayu <614422099@qq.com>
2019-01-23 18:24:19 +0800

    * adbkit EncodeCommend add comment

--- adbkit/unit.go
+++ adbkit/unit.go
@@ -5,9 +5,11 @@ import (
 	"strings"
 )
 
+// 对命令进行编码
 func EncodeCommend(command string) []byte {
 	prefix := strings.ToUpper("0000" + fmt.Sprintf("%X", len(command)))
 	length := len(prefix)
 	prefix = prefix[length-4 : length]
+	fmt.Println(prefix + command)
 	return []byte(prefix + command)
 }
