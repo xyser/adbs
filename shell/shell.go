baa140e9c210743d3108ef0c01ee78c450d273b9
dingdayu <614422099@qq.com>
2019-01-25 11:25:13 +0800

    * shell package add adb check
    Signed-off-by: dingdayu <614422099@qq.com>

--- shell/shell.go
+++ shell/shell.go
@@ -1,7 +1,9 @@
 package shell
 
 import (
+	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/gorilla/websocket"
 	"github.com/kr/pty"
@@ -9,9 +11,16 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	_ "os/signal"
 	"syscall"
 	"unsafe"
 )
+
+func init() {
+	if ok, err := checkAdb(); !ok || err != nil {
+		log.Fatal("No find adb: " + err.Error())
+	}
+}
 
 type windowSize struct {
 	Rows uint16 `json:"rows"`
@@ -110,4 +119,29 @@ func Shell(conn *websocket.Conn) {
 			log.Printf("Error: Unknown data type[%d]\n", dataTypeBuf[0])
 		}
 	}
+}
+
+func checkAdb() (bool, error) {
+	cmd := exec.Command("which", "adb")
+
+	//读取io.Writer类型的cmd.Stdout，再通过bytes.Buffer(缓冲byte类型的缓冲器)将byte类型转化为string类型(out.String():这是bytes类型提供的接口)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+
+	//Run执行c包含的命令，并阻塞直到完成。  这里stdout被取出，cmd.Wait()无法正确获取stdin,stdout,stderr，则阻塞在那了
+	if err := cmd.Run(); err != nil {
+		return false, err
+	}
+	path := bytes.Trim(out.Bytes(), "\n")
+
+	info, err := os.Stat(string(path))
+	if err != nil {
+		return false, err
+	}
+
+	if uint32(info.Mode().Perm()&os.FileMode(73)) == uint32(73) {
+
+		return true, nil
+	}
+	return false, errors.New(fmt.Sprintf("[%s] no execution permission", path))
 }
