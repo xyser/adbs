99e315fcf3e0b6bbacb47b9d1c9e0c72177f3ccb
dingdayu <614422099@qq.com>
2019-01-28 18:13:06 +0800

    * remove comment
    Signed-off-by: dingdayu <614422099@qq.com>

--- main.go
+++ main.go
@@ -1,34 +1,8 @@
 package main
 
-import (
-	"adbs/adbkit"
-	"fmt"
-)
+import "adbs/api"
 
 func main() {
-	//api.Init()
-
-	//response, _ := adbkit.New("127.0.0.1", 5037).Command("host:devices")
-	//fmt.Println(string(response))
-
-	fmt.Println(adbkit.New("127.0.0.1", 5037).Devices())
-
-	//// adb version
-	//version, err := shell.Version()
-	//fmt.Println(version)
-	//
-	//// 软件包列表
-	//pack, err := packages.List()
-	//fmt.Println(pack)
-	//
-	//// 清理软件包缓存
-	//if bo, err = packages.Clear("com.dingdayu.helloandriod"); err != nil {
-	//	fmt.Println(err)
-	//}
-	//fmt.Printf("Packages.Clear: %s\n", bo)
-	//
-	//if bo, err = packages.StartByIntent("com.dingdayu.helloandroid/.MainActivity", map[string]string{}); err != nil {
-	//	fmt.Println(err)
-	//}
-	//fmt.Printf("Packages.StartByIntent: %s\n", bo)
+	// 启动API
+	api.Init()
 }
